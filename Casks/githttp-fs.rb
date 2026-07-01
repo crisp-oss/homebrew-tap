db3b4984cc550a0b2abf573d5e77515c5865675c
Valerian Saliou <valerian@valeriansaliou.name>
2026-07-01 18:22:15 +0100

    githttp-fs v1.0.4

--- Casks/githttp-fs.rb
+++ Casks/githttp-fs.rb
@@ -1,10 +1,10 @@
 cask "githttp-fs" do
-  version "1.0.3"
+  version "1.0.4"
 
   arch arm: "aarch64"
   platform = "macos"
 
-  sha256 arm: "102383828329015935bef5371b3a4c154a11a31901810fd7d6bad461074be4ce"
+  sha256 arm: "5d924e44bff31fb0fd3fc9af1d9e6a9ba1291c2d8e4d7b86e7de103a0e3ed7f7"
 
   url "https://github.com/crisp-oss/homebrew-tap/releases/download/githttp-fs/githttp-fs-#{version}-#{platform}-#{arch}.zip"
   name "githttp-fs"
